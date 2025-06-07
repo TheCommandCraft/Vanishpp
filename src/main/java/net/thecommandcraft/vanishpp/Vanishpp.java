51b55aa13ecedb7fa1aac7dca4a1212e3816b7e2
TCC <thecommandcraft.studios@gmail.com>
2025-06-07 02:08:46 +0200

    Initial commit

---
+++ src/main/java/net/thecommandcraft/vanishpp/Vanishpp.java
@@ -0,0 +1,17 @@
+package net.thecommandcraft.vanishpp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Vanishpp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
